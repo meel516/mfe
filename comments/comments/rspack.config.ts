0e24ab3b0243b983903cf93ad0f0f33c5fe4861d
meel516 <102860983+meel516@users.noreply.github.com>
2025-06-30 00:53:16 +0530

    fix: update publicPath in rspack configuration to use 'auto' instead of localhost URL

--- comments/comments/rspack.config.ts
+++ comments/comments/rspack.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     // You need to set a unique value that is not equal to other applications
     uniqueName: "comments",
     // publicPath must be configured if using manifest
-    publicPath: "http://localhost:3100/",
+    publicPath: "auto",
   },
 
   experiments: {
